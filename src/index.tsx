77e05638461945b29d8c857899cfbb91a596a8a5
aletheia <bianchi.luca@gmail.com>
2020-08-12 17:57:44 +0200

    :sparkles: Add index file. refs #1
    Add intex, style and component creation

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+import { BoxAnimation } from "./components/Animation";
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BoxAnimation>
+      <App />
+    </BoxAnimation>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
