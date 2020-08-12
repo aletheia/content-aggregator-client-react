1e8031b9e53556ef8dae35bcc285b75831540c71
aletheia <bianchi.luca@gmail.com>
2020-08-12 23:32:40 +0200

    :sparkles: Add Blog. refs #1
    Added Blog component with Title and Entry

---
+++ src/components/Blog/Title/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './title.scss';
+
+export const Title = () => {
+  return (
+    <div className="section">
+      <h1>
+        make everything
+        <br />
+        serverless
+      </h1>
+    </div>
+  );
+};
